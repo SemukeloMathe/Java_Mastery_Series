fc315655af1b32dfc49e8a2224c2045ed9a24b0a
SemTheLegend <semmathe@gmail.com>
2024-08-07 18:53:21 +0200

    first java program

--- Hello.java
+++ Hello.java
@@ -1,10 +1,9 @@
 class Hello {
+
   public static void main(String a[]) {
-    int num1 = 3;
-    int num2 = 5;
-    int result = num1 + num2;
-    num2 = 10;
-    result = num1 + num2;
-    System.out.print(result);
+    
+    System.out.print("Hello World");
+
   }
+  
 }
