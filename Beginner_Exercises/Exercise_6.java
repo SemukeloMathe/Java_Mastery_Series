e699d840613c9150ae5ebdec4ad217c1607e5319
SemTheLegend <semmathe@gmail.com>
2024-08-13 17:50:49 +0200

    exercise 6 solution

--- Beginner_Exercises/Exercise_6.java
+++ Beginner_Exercises/Exercise_6.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
 public class Exercise_6 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter a number");
+    System.out.print("Enter a number");
     
     int num = in.nextInt();
     int i = 1;
